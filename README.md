f57073386233a5c2be9dd2f1830022440e48d20b
Steven Shoemaker <stshoema@ucsd.edu>
2021-11-07 23:04:39 -0800

    Finished the project, inclusive of service workers

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Lab 7 - Starter Code
+# Lab 7
+
+### Group Members
+- Steven Shoemaker
+
+## Link to Github Pages
+https://ucsdsteveshoe.github.io/Lab7_Starter/
--- sw.js
+++ sw.js
@@ -3,12 +3,35 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+const urlsToCache = [
+  'https://introweb.tech/assets/json/ghostCookies.json',
+  'https://introweb.tech/assets/json/birthdayCake.json',
+  'https://introweb.tech/assets/json/chocolateChip.json',
+  'https://introweb.tech/assets/json/stuffing.json',
+  'https://introweb.tech/assets/json/turkey.json',
+  'https://introweb.tech/assets/json/pumpkinPie.json'
+];
+
+/*
+[
+  '/',
+  '/assets/scripts/main.js',
+  '/assets/scripts/Router.js',
+  '/assets/styles/main.css'
+]
+*/
+
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
-  /**
-   * TODO - Part 2 Step 2
-   * Create a function as outlined above
-   */
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  console.log('finished cache installation')
 });
 
 /**
@@ -17,16 +40,40 @@ self.addEventListener('install', function (event) {
  * go through this service worker
  */
 self.addEventListener('activate', function (event) {
-  /**
-   * TODO - Part 2 Step 3
-   * Create a function as outlined above, it should be one line
-   */
+  event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
 self.addEventListener('fetch', function (event) {
-  /**
-   * TODO - Part 2 Step 4
-   * Create a function as outlined above
-   */
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
