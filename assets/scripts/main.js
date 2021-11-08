e618e9aa69e891989e34f6dce269807a9a5c3d37
Steven Shoemaker <stshoema@ucsd.edu>
2021-11-07 23:50:01 -0800

    Last attempt to get SW working properly

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,11 +53,10 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
-  console.log('No service workers registered');
-  /*
+  console.log('SW initializing');
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('./../../sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
@@ -65,7 +64,7 @@ function initializeServiceWorker() {
         console.log('ServiceWorker registration failed: ', err);
       });
     });
-  } */
+  }
 }
 
 /**
