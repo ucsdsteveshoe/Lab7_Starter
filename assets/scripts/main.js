5b3035941435b0371404c1b00a2a768be050d862
Steven Shoemaker <stshoema@ucsd.edu>
2021-11-07 23:53:14 -0800

    Fix for SW not loading on GH

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,9 +53,10 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
+  console.log('Init service worker');
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
